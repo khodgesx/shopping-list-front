339e58c6c952a62f37f1b0c654a61d0eece593e1
khodges <zkellyhodges@gmail.com>
2022-06-24 12:19:49 -0700

    set up homepage file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import emoji from './images/glasses-emoji.jpeg'
+import Home from './homepage/home'
+
+
+function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/homepage/home.jsx
@@ -0,0 +1,18 @@
+import '../App.css'
+//import emoji from '../images/gasses-emoji.jpeg'
+
+const Home = () =>{
+    return(
+        <div className="App">
+            <h1>List Baby</h1>
+            <p>Love a good list? Keep track of all your lists here
+            {/* <img id="glasses-emoji"src={emoji} alt="glasses emoji" /> */}
+               
+            </p>
+
+        </div>
+    )
+}
+
+export default Home;
+
