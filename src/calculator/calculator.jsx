c1e571305e9c0c7afd1f65990ca55b82c509470d
khodges <zkellyhodges@gmail.com>
2022-08-26 10:21:22 -0700

    calculator functionality working except need to add decimal and adding more operations to the sequence if user does not hit equals first

--- src/calculator/calculator.jsx
+++ src/calculator/calculator.jsx
@@ -16,7 +16,7 @@ const Calculator = () =>{
     const [waitingforSecond, setWaitingForSecond] = useState(false)
     const [secondOperand, setSecondOperand] = useState(0)
 
-    const [waitForOperator, setWaitForOperator] = useState(false)
+    const [waitForOperator, setWaitForOperator] = useState(true)
     const [operator, setOperator] = useState('')
 
     const [result, setResult] = useState(0)
@@ -26,26 +26,29 @@ const Calculator = () =>{
     const evaluateNums =(e)=>{
         if(waitingforSecond === false){
             if(firstOperand === 0){
-                console.log(e.target.innerHTML)
                 setFirstOperand(e.target.innerHTML)
-                setScreen(firstOperand)
+                setScreen(e.target.innerHTML)
             }else{
                 setFirstOperand(firstOperand + e.target.innerHTML)
-                setScreen(firstOperand)
+                setScreen(firstOperand + e.target.innerHTML)
             }
+            
+            //setScreen(firstOperand)
         }else{ // we are on the second operand now:
             if(secondOperand === 0){
-                console.log(e.target.innerHTML)
                 setSecondOperand(e.target.innerHTML)
+                setScreen(e.target.innerHTML)
             }else{
                 setSecondOperand(secondOperand + e.target.innerHTML)
+                setScreen(secondOperand + e.target.innerHTML)
             } 
         }
     }
 
     const evaluateOperator = (e)=>{
-        if(waitForOperator === false){
+        if(waitForOperator === true){
             setOperator(e.target.innerHTML)
+            setScreen(e.target.innerHTML)
             setWaitingForSecond(true)
             setWaitForOperator(false)
         }
@@ -54,25 +57,44 @@ const Calculator = () =>{
     const solve = ()=>{
         if(!isNaN(firstOperand) && !isNaN(secondOperand)){
             if(operator === '+'){
-                setResult(parseInt(firstOperand) + parseInt(secondOperand))
-                setFirstOperand(result)
-                setWaitForOperator(true)
+                let answer = parseInt(firstOperand) + parseInt(secondOperand)
+                setResult(answer)
+                setScreen(answer)
+                setFirstOperand(answer)
             }else if(operator === '-'){
-                setResult(parseInt(firstOperand) - parseInt(secondOperand))
-                setFirstOperand(result)
-                setWaitForOperator(true)
+                let answer = parseInt(firstOperand) - parseInt(secondOperand)
+                setResult(answer)
+                setScreen(answer)
+                setFirstOperand(answer)
             }else if(operator === '*'){
-                setResult(parseInt(firstOperand) * parseInt(secondOperand))
-                setFirstOperand(result)
-            setWaitForOperator(true)
+                let answer = parseInt((firstOperand) * parseInt(secondOperand))
+                setResult(answer)
+                setScreen(answer)
+                setFirstOperand(answer)
+                setScreen(answer)
             }else if(operator === '/'){
-                setResult(parseInt(firstOperand) / parseInt(secondOperand))
-                setFirstOperand(result)
-            setWaitForOperator(true)
+                let answer = parseInt(firstOperand) / parseInt(secondOperand)
+                setResult(answer)
+                setScreen(answer)
+                setFirstOperand(answer)
             }else{
-                setResult(0)
+                //setResult(0)
             }
+            setWaitForOperator(true)
+            setWaitingForSecond(true)
+            setSecondOperand(0)
+            
         }
+        
+    }
+    const clear=()=>{
+        setResult(0)
+        setScreen(0)
+        setFirstOperand(0)
+        setSecondOperand(0)
+        setWaitingForSecond(false)
+        setWaitForOperator(false)
+        setOperator('')
         
     }
 
@@ -84,30 +106,31 @@ const Calculator = () =>{
                 <button onClick={evaluateNums}className="numbers" id="8">8</button>
                 <button onClick={evaluateNums}className="numbers" id="9">9</button>
 
-                <button onClick={evaluateOperator}className="numbers" id="/">/</button>
+                <button onClick={evaluateOperator}className="numbers op" id="/">/</button>
 
                 <button onClick={evaluateNums}className="numbers" id="4">4</button>
                 <button onClick={evaluateNums}className="numbers" id="5">5</button>
                 <button onClick={evaluateNums}className="numbers" id="6">6</button>
 
-                <button onClick={evaluateOperator}className="numbers" id="*">*</button>
+                <button onClick={evaluateOperator}className="numbers op" id="*">*</button>
 
                 <button onClick={evaluateNums}className="numbers" id="1">1</button>
                 <button onClick={evaluateNums}className="numbers" id="2">2</button>
                 <button onClick={evaluateNums}className="numbers" id="3">3</button>
 
-                <button onClick={evaluateOperator}className="numbers" id="-">-</button>
+                <button onClick={evaluateOperator}className="numbers op" id="-">-</button>
 
                 <button onClick={evaluateNums}className="numbers" id="0">0</button>
 
                 <button className="numbers" id=".">.</button>
 
-                <button onClick={solve}className="numbers" id="=">=</button>
+                <button onClick={solve}className="numbers" id="equals">=</button>
 
-                <button onClick={evaluateOperator}className="numbers" id="+">+</button>
+                <button onClick={evaluateOperator}className="numbers op" id="+">+</button>
             </div>
             <div className="calc-entry">
                 <input id="screen"type="text" value={screen}disabled></input>
+                <button onClick={clear}>Clear</button>
                 
                 <h3>First Operand = {firstOperand}</h3>
                 <h3>operator: {operator}</h3>
